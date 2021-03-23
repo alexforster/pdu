17a10d9054efcab9b92252838540f430c32e3476
Alex Forster <aforster@cloudflare.com>
2021-03-23 17:51:10 -0500

    Add convenience scripts for running tests and fuzzing using Docker containers

---
+++ fuzz.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+DOCKER="docker"
+
+${DOCKER} build -t pdu-fuzz - <<'EOF'
+FROM ubuntu:bionic
+ENV LANG=C.UTF-8 \
+    LC_ALL=C.UTF-8
+VOLUME /usr/local/src/pdu
+WORKDIR /usr/local/src/pdu
+SHELL ["/bin/bash", "-eu", "-o", "pipefail", "-c"]
+RUN \
+	export DEBIAN_FRONTEND=noninteractive; \
+	apt-get -q update; \
+	apt-get -q install -y curl build-essential linux-headers-generic pkg-config binutils-dev libunwind-dev libpcap-dev tshark; \
+	apt-get -q clean autoclean;
+RUN \
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable; \
+	source $HOME/.cargo/env; \
+	cargo install honggfuzz;
+ENTRYPOINT \
+	source $HOME/.cargo/env; \
+	cd ./fuzz; \
+	RUSTFLAGS="-C link-dead-code" cargo hfuzz run $FUZZ_TARGET
+EOF
+
+if [ -z "$1" ]; then
+  echo "Usage: fuzz.sh [ ethernet | arp | ipv4 | ipv6 | tcp | udp | icmp | gre ]"
+fi
+
+${DOCKER} run --init --rm -v "$(pwd):/usr/local/src/pdu" -e FUZZ_TARGET=$1 pdu-fuzz
