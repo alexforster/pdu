e2488acff7bc23cc3a6d8a1e33c2460198148f85
Alex Forster <aforster@cloudflare.com>
2021-06-02 15:48:25 -0500

    Add 'clean' target to fuzz.sh

--- fuzz.sh
+++ fuzz.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+if [ "clean" = "$1" ]; then
+  echo $(pwd)/fuzz/hfuzz_target
+  rm -rf $(pwd)/fuzz/hfuzz_target
+  for FILE in $(pwd)/fuzz/hfuzz_workspace/*/*.*; do
+    echo $FILE
+    rm -f $FILE
+  done
+  exit
+fi
+
 DOCKER="docker"
 
 ${DOCKER} build -t pdu-fuzz - <<'EOF'
@@ -10,22 +20,23 @@ VOLUME /usr/local/src/pdu
 WORKDIR /usr/local/src/pdu
 SHELL ["/bin/bash", "-eu", "-o", "pipefail", "-c"]
 RUN \
-	export DEBIAN_FRONTEND=noninteractive; \
-	apt-get -q update; \
-	apt-get -q install -y curl build-essential linux-headers-generic pkg-config binutils-dev libunwind-dev libpcap-dev tshark; \
-	apt-get -q clean autoclean;
+  export DEBIAN_FRONTEND=noninteractive; \
+  apt-get -q update; \
+  apt-get -q install -y curl build-essential linux-headers-generic pkg-config binutils-dev libunwind-dev libpcap-dev tshark; \
+  apt-get -q clean autoclean;
 RUN \
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable; \
-	source $HOME/.cargo/env; \
-	cargo install honggfuzz;
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable; \
+  source $HOME/.cargo/env; \
+  cargo install honggfuzz;
 ENTRYPOINT \
-	source $HOME/.cargo/env; \
-	cd ./fuzz; \
-	RUSTFLAGS="-C link-dead-code" cargo hfuzz run $FUZZ_TARGET
+  mkdir -p /tmp/honggfuzz/$FUZZ_TARGET; \
+  source $HOME/.cargo/env; \
+  cd ./fuzz; \
+  RUSTFLAGS="-C link-dead-code" HFUZZ_RUN_ARGS="-t 5 -T --output /tmp/honggfuzz/$FUZZ_TARGET" cargo hfuzz run $FUZZ_TARGET
 EOF
 
 if [ -z "$1" ]; then
-  echo "Usage: fuzz.sh [ ethernet | arp | ipv4 | ipv6 | tcp | udp | icmp | gre ]"
+  echo "Usage: fuzz.sh [ clean | ethernet | arp | ipv4 | ipv6 | tcp | udp | icmp | gre ]"
 fi
 
 ${DOCKER} run --init --rm -v "$(pwd):/usr/local/src/pdu" -e FUZZ_TARGET=$1 pdu-fuzz
