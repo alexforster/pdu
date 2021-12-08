bc7092583f10c648d93fc5fd905b985982faa990
Alex Forster <aforster@cloudflare.com>
2021-12-08 11:18:22 -0600

    Release 1.4.2

--- test.sh
+++ test.sh
@@ -10,17 +10,17 @@ VOLUME /usr/local/src/pdu
 WORKDIR /usr/local/src/pdu
 SHELL ["/bin/bash", "-eu", "-o", "pipefail", "-c"]
 RUN \
-	export DEBIAN_FRONTEND=noninteractive; \
-	apt-get -q update; \
-	apt-get -q install -y curl build-essential linux-headers-generic pkg-config binutils-dev libunwind-dev libpcap-dev tshark; \
-	apt-get -q clean autoclean;
+  export DEBIAN_FRONTEND=noninteractive; \
+  apt-get -q update; \
+  apt-get -q install -y curl build-essential linux-headers-generic pkg-config binutils-dev libunwind-dev libpcap-dev tshark; \
+  apt-get -q clean autoclean;
 RUN \
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable; \
-	source $HOME/.cargo/env; \
-	cargo install honggfuzz;
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable; \
+  source $HOME/.cargo/env; \
+  cargo install honggfuzz;
 ENTRYPOINT \
-	source $HOME/.cargo/env; \
-	cargo test --verbose
+  source $HOME/.cargo/env; \
+  cargo test --verbose
 EOF
 
 ${DOCKER} run --init --rm -v "$(pwd):/usr/local/src/pdu" pdu-test
